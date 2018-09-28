80a039e771f72f7b2a185aeaa9bcbb929d627b88
Salvador Guzman <guzmansalv@gmail.com>
2018-09-28 10:12:43 -0700

    raw dump of another project

---
+++ src/main.py
@@ -0,0 +1,89 @@
+import bs4
+import requests
+import queue
+import threading
+import psycopg2
+import json
+import random
+from multiprocessing.dummy import Pool
+
+
+seen = queue.Queue()
+cores = 8
+pool = Pool(cores)
+limit = 15865
+
+
+def insert_vids(cursor, data, table_chan):
+    sql_insert_chann = 'INSERT INTO youtube.channels.chans ' \
+                       '(channel_serial, title, custom_url, description, joined, thumbnail, topic_ids, ' \
+                       'topic_categories, privacy_status, is_linked, long_uploads, tracking_id, ' \
+                       'moderate_comments, show_related_channels, show_browse, banner_image) ' \
+                       'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+
+    for datum in data:
+        if datum not in table_chan:
+            print(datum)
+            cursor.execute(sql_insert_chann, [datum])
+
+
+def seen_daemon():
+    conn = psycopg2.connect(user='root', password='', host='127.0.0.1', port='5432', database='youtube')
+    table_chans = get_incumbent_chans(conn)
+
+    while True:
+        idx, channels = seen.get(block=True)
+        cursor = conn.cursor()
+        insert_vids(cursor, channels, table_chans)
+        conn.commit()
+        cursor.close()
+
+
+def page(idx):
+    url = f'https://dbase.tube/chart/channels/subscribers/all?page={idx}&spf=navigate'
+    return requests.get(url)
+
+
+def soup_page(idx):
+    req = page(idx)
+    text = req.text
+    json_data = json.loads(text)
+    body = json_data['body']['spf_content']
+
+    return bs4.BeautifulSoup(body, 'html.parser')
+
+
+def vids(i):
+    pg = soup_page(i)
+    channels = []
+    select = pg.select('a.aj.row')
+    for a_href in select:
+        path = a_href['href'].split('/')
+        channels.append(path[2])
+
+    seen.put((i, channels))
+
+
+def get_incumbent_chans(conn):
+    postgresql_select_query = 'SELECT channel_id FROM youtube.channels.channels'
+    cursor = conn.cursor()
+    cursor.execute(postgresql_select_query)
+    records = cursor.fetchall()
+
+    ignore = set()
+    for i in records:
+        ignore.add(i[0])
+
+    print(len(ignore), 'channels from table')
+
+    cursor.close()
+    return ignore
+
+
+def main():
+    threading.Thread(target=seen_daemon, daemon=True).start()
+    pool.map(vids, range(limit))
+    print('done')
+
+
+main()
